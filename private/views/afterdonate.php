678f2e63b573d988fe24eb143dd154abdeb79eec
Pasan Malindu <malindupasan@gmail.com>
2023-02-03 15:48:48 +0530

    finished become a donor component

---
+++ private/views/afterdonate.php
@@ -0,0 +1,48 @@
+<?php $this->view("includes/navbar", ); ?>
+<?php $i=0?>
+<link rel="stylesheet" href="<?= ROOT ?>/css/wheretodonate.css">
+
+<div class="sec1">
+    <h1>Become a Donor</h1>
+</div>
+
+<div class="sec2">
+<a class="a" href="<?=ROOT?>/becomeadonor">Ways to Donate Blood</a>
+    <a class="a" href="<?=ROOT?>/becomeadonor?click=amieligible">Am I Eligible?</a>
+    <a class="a" href="<?=ROOT?>/becomeadonor?click=wheretodonate">Where to Donate</a>
+    <a class="a" href="<?=ROOT?>/becomeadonor?click=whendonating">What to Expect When Donating?</a>
+    <a class="a" href="<?=ROOT?>/becomeadonor?click=beforedonate">Before you Donate</a>
+    <a class="a1" href="<?=ROOT?>/becomeadonor?click=afterdonate">After you Donate</a>
+</div>
+
+<div class="sec3">
+    <div class="sec3-h">
+        <p>In the next 1-2 hours...</p>
+        <ul>
+            <li>Make sure to appropriately rest and avoid excessive movement of your arms</li>
+            <li>Avoid carrying heavy loads on the arm used for your donation</li>
+            <li>Drink at least 2 glasses of water (approx. 500mL), then continue to drink plenty of fluids throughout the day</li>
+            <li>Avoid drinking alcohol or smoking as this may lead to lightheadedness and fainting</li>
+        </ul>
+    </div>
+    <div class="sec3-h">
+        <p>In the next 6 hours...</p>
+        <ul>
+            <li>Do not rush around or walk for long periods</li>
+            <li>Do not stand for long periods</li>
+            <li>Do not do any strenuous exercise (e.g. jogging, cycling, or going to the gym)</li>
+            <li>Do not do any strenous or hazardous occupational activities (e.g. lifting heavy loads)</li>
+            <li>Do not have a hot shower or bath</li>
+        </ul>
+    </div>
+
+
+
+       
+  
+   
+</div>
+
+
+
+
