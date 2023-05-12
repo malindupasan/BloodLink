a99cd9f632db96bd1cf61015814fd2c0dbdc00e4
Pasan Malindu <malindupasan@gmail.com>
2023-05-12 16:28:17 +0530

    added 3 month constraint

--- private/controllers/Checkpreviouscamps.php
+++ private/controllers/Checkpreviouscamps.php
@@ -5,8 +5,15 @@ class Checkpreviouscamps extends Controller
     {
 
         $cmp=new Camp();
-        $qry="select c.camp_id,c.latitude,c.longitude from blood_donation_camp c "; 
-       $data=$cmp->query($qry);
+        $currentDate = date('Y-m-d'); 
+        $threeMonthsAgo = strtotime('-3 months', strtotime($currentDate));
+        $arr=array();
+       
+        $threeMonthsAgoFormatted = date('Y-m-d', $threeMonthsAgo);
+        $arr['date']=$threeMonthsAgoFormatted; //get the date before 3 months from today
+        $qry="select c.camp_id,c.latitude,c.longitude from blood_donation_camp c where c.date>=:date"; 
+       $data=$cmp->query($qry,$arr);
+    //    print_r($data);
        
  
        $d2=$cmp->findCampsWithinRadius($data,6.902642194125529,79.86046625449221,205);
--- private/views/wheretodonate.php
+++ private/views/wheretodonate.php
@@ -1,9 +1,9 @@
 <?php $this->view("includes/navbar", ); ?>
 <title>Where to Donate?</title>
-<?php $i=0?>
+<?php $i = 0 ?>
 <link rel="stylesheet" href="<?= ROOT ?>/css/wheretodonate.css">
 <?php
-$data=$data[0];
+$data = $data[0];
 // print_r($data); 
 ?>
 <div class="sec1">
@@ -11,17 +11,18 @@ $data=$data[0];
 </div>
 
 <div class="sec2">
-<a class="a" href="<?=ROOT?>/becomeadonor">Ways to Donate Blood</a>
-    <a class="a" href="<?=ROOT?>/becomeadonor?click=amieligible">Am I Eligible?</a>
-    <a class="a1" href="<?=ROOT?>/becomeadonor?click=wheretodonate">Where to Donate</a>
-    <a class="a" href="<?=ROOT?>/becomeadonor?click=whendonating">What to Expect When Donating?</a>
-    <a class="a" href="<?=ROOT?>/becomeadonor?click=beforedonate">Before you Donate</a>
-    <a class="a" href="<?=ROOT?>/becomeadonor?click=afterdonate">After you Donate</a>
+    <a class="a" href="<?= ROOT ?>/becomeadonor">Ways to Donate Blood</a>
+    <a class="a" href="<?= ROOT ?>/becomeadonor?click=amieligible">Am I Eligible?</a>
+    <a class="a1" href="<?= ROOT ?>/becomeadonor?click=wheretodonate">Where to Donate</a>
+    <a class="a" href="<?= ROOT ?>/becomeadonor?click=whendonating">What to Expect When Donating?</a>
+    <a class="a" href="<?= ROOT ?>/becomeadonor?click=beforedonate">Before you Donate</a>
+    <a class="a" href="<?= ROOT ?>/becomeadonor?click=afterdonate">After you Donate</a>
 </div>
 
 <div class="sec3">
     <div class="sec3-h">
-        <p>You can donate blood by going to any blood bank or any <a href="<?= ROOT ?>/campaigns"> campaign</a> near your
+        <p>You can donate blood by going to any blood bank or any <a href="<?= ROOT ?>/campaigns"> campaign</a> near
+            your
             area. </p>
     </div>
     <div class="sec3-h">
@@ -31,33 +32,91 @@ $data=$data[0];
 
 <div class="sec4">
 
-<?php foreach($data as $value):?>
-            
-            <?php if($i%3==0 && $i!=0) : ?>
-                    </div>
-                    <div class="sec4"> 
-            <?php endif ?>
+    <?php foreach ($data as $value): ?>
 
-<div class="card"><a href="<?=ROOT?>/bloodbankpage/<?=$value->blood_bank_id?>" >
-    <div class="loc-logo">
-    <img src="<?=ROOT?>/images/location.png" alt="">
-    </div>
-    </a>
-    <div class="info">
-        <div class="bname"><?=$value->name?></div>
-        <div class="baddress1"><?=$value->house_no?> <?=$value->street?>,</div>
-        <div class="baddress2"><?=$value->city?></div>
-        <div class="btel">Tel : <?=$value->telephone?></div>
-    </div>
-   
-   
-</div>
-<?php $i++; ?>
-            <?php endforeach; ?>
+        <?php if ($i % 3 == 0 && $i != 0): ?>
+        </div>
+        <div class="sec4">
+        <?php endif ?>
+
+        <div class="card"><a href="<?= ROOT ?>/bloodbankpage/<?= $value->blood_bank_id ?>">
+                <div class="loc-logo">
+                    <img src="<?= ROOT ?>/images/location.png" alt="">
+                </div>
+            </a>
+            <div class="info">
+                <div class="bname"><?= $value->name ?></div>
+                <div class="baddress1"><?= $value->house_no ?>     <?= $value->street ?>,</div>
+                <div class="baddress2"><?= $value->city ?></div>
+                <div class="btel">Tel : <?= $value->telephone ?></div>
             </div>
-            
+
+
+        </div>
+        <?php $i++; ?>
+    <?php endforeach; ?>
+</div>
+
+
+
+
 
 
 </div>
 
+<div class="map" id="map"></div>
 
+<script>
+    
+
+
+function initAutocomplete() {
+    let srilanka = { lat: 7.8731, lng: 80.7718 };
+    // The location of Uluru
+
+    // The map, centered at Uluru
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: srilanka,
+        mapTypeControl: false,
+
+
+    });
+    var locations=[];
+    <?php foreach ($data as $key):  ?>
+  
+        
+
+    locations.push({lat:parseFloat(<?= $key['latitude'] ?>), lng:parseFloat(<?= $key['longitude'] ?>)});
+    
+    
+    
+
+     <?php endforeach; ?> 
+     console.log(locations)
+     locations.forEach(function(location) {
+        new google.maps.Marker({
+          position: location,
+          map: map
+        });
+      });
+
+
+
+    // var marker = new google.maps.Marker({
+    //     draggable: true,//false
+
+    //     map: map
+    // });
+    // marker.setPosition(srilanka); //change
+    // marker.setVisible(true); //after
+    
+ 
+
+}
+//here
+
+document.addEventListener("DOMContentLoaded", function (event) { initAutocomplete() });
+
+
+</script>
